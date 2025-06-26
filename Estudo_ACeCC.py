bc30e6ea5fd68cb993c54394cbe6ba012befeae7
RafaelCoutinh0 <rafaelcoutinho@ufba.br>
2025-06-26 20:00:00 -0300

    Adicionando pastas Correlações e Autocorrelações

---
+++ Estudo_ACeCC.py
@@ -0,0 +1,86 @@
+import os
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from statsmodels.tsa.stattools import ccf, acf
+from itertools import combinations
+from initialization_oil_production_basic import plotar_graficos
+
+# === NOME DAS VARIÁVEIS ===
+xf_names = [
+    'P_man', 'q_transp',
+    'P_fbhp_1', 'P_choke_1', 'q_mean_1',
+    'P_fbhp_2', 'P_choke_2', 'q_mean_2',
+    'P_fbhp_3', 'P_choke_3', 'q_mean_3',
+    'P_fbhp_4', 'P_choke_4', 'q_mean_4'
+]
+zf_names = [
+    'P_intake_1', 'dP_bcs_1',
+    'P_intake_2', 'dP_bcs_2',
+    'P_intake_3', 'dP_bcs_3',
+    'P_intake_4', 'dP_bcs_4'
+]
+# === GERAR DADOS DE SIMULAÇÃO ===
+print("Executando simulação para obter dados...")
+n_pert = 10  # número de perturbações (ajustável)
+Lista_xf_reshaped, Lista_zf_reshaped = plotar_graficos(n_pert)  # isso popula Lista_xf_reshaped e Lista_zf_reshaped
+
+# === CRIAR DATAFRAME ===
+def criar_dataframe(Lista_xf_reshaped, Lista_zf_reshaped):
+    all_data = np.vstack([Lista_xf_reshaped, Lista_zf_reshaped])
+    all_names = xf_names + zf_names
+    df = pd.DataFrame(all_data.T, columns=all_names)
+    return df
+
+df = criar_dataframe(Lista_xf_reshaped, Lista_zf_reshaped)
+# === AUTOCORRELAÇÃO ===
+import matplotlib.pyplot as plt
+def plot_autocorrelacoes(df):
+    pasta = 'Autocorrelações'
+    for col in df.columns:
+        var = df[col][::2]
+        acf_var = acf(var,fft=False,nlags=len(var)-1)
+        plt.figure()
+        plt.plot(acf_var, '.')
+        plt.title(f'Autocorrelação - {col}')
+        nome_autocorrelacoes = f"autocorrelacao_{col}"
+        caminho_completo = os.path.join(pasta, nome_autocorrelacoes)
+        plt.savefig(caminho_completo)
+        plt.close()
+
+# === CORRELAÇÃO CRUZADA ===
+def plot_correlacoes_cruzadas(df):
+    pasta = 'Correlações'
+    pairs = list(combinations(df.columns, 2))
+    for var1, var2 in pairs:
+        x = df[var1] - df[var1].mean()
+        y = df[var2] - df[var2].mean()
+        ccf_vars = ccf(x,y,nlags=len(x)-1)
+        plt.figure()
+        plt.plot(ccf_vars,'.')
+        plt.title(f'Correlação cruzada - {var1} vs {var2}')
+        nome_correlacoes = f'{var1} vs {var2}'
+        caminho_completo = os.path.join(pasta, nome_correlacoes)
+        plt.savefig(caminho_completo)
+        plt.close()
+# === MATRIZ DE CORRELAÇÃO ===
+def matriz_correlacao(df):
+    nome_matriz = "matriz_correlacao.png"
+    pasta = 'Correlações'
+    corr = df.corr()
+    plt.figure(figsize=(28, 20))
+    sns.heatmap(corr, annot=True, fmt=".2f", cmap='coolwarm', square=True)
+    plt.title("Matriz de Correlação (Lag 0)")
+    plt.tight_layout()
+    caminho_completo = os.path.join(pasta, nome_matriz)
+    plt.savefig(caminho_completo)
+    plt.close()
+# === EXECUÇÃO DAS ANÁLISES ===
+print("Plotando autocorrelações...")
+plot_autocorrelacoes(df)
+
+print("Plotando correlações cruzadas...")
+plot_correlacoes_cruzadas(df)
+
+print("Plotando matriz de correlação...")
+matriz_correlacao(df)
--- initialization_oil_production_basic.py
+++ initialization_oil_production_basic.py
@@ -15,8 +15,6 @@ import matplotlib.pyplot as plt
 import matplotlib.pyplot
 from matplotlib import rcParams
 from casadi import MX, interpolant, Function, sqrt, vertcat, integrator, jacobian, transpose
-# from scipy.special import functions
-from plotgraf import *
 from bcs_models import *
 from manifold import *
 from numpy import linspace, array, eye, zeros, repeat, concatenate, delete, diag
@@ -195,7 +193,7 @@ z += [P_intake_4, dP_bcs_4]
 mani_model = mani.model(0, x, z, u)
 
 # %% Evaluation of steady-state
-u0 = [56., 10 ** 5, 50., .5, 50., .5, 50., .5, 50., .5]
+u0 = [56., 10 ** 4, 50., .5, 50., .5, 50., .5, 50., .5]
 
 x0 = [76.52500, 4 * 85,
       64.11666, 120.91641, 85,
@@ -221,8 +219,8 @@ dae = {'x': vertcat(*x), 'z': vertcat(*z), 'p': vertcat(*u), 'ode': vertcat(*man
        'alg': vertcat(*mani_model[-8:])}
 
 tfinal = 1000 # [h]
-
-grid = linspace(0, tfinal, 100)
+qtd_pts  = 25
+grid = linspace(0, tfinal, qtd_pts)
 
 F = integrator('F', 'idas', dae, 0, grid)
 
@@ -245,29 +243,32 @@ def plotar_graficos(n_pert):
     map_est.append(z0)
     Lista_zf = np.array(Lista_zf)
     Lista_xf = np.array(Lista_xf)
-    Lista_zf_reshaped = Lista_zf.reshape(8, 100)
-    Lista_xf_reshaped = Lista_xf.reshape(14, 100)
+    Lista_zf_reshaped = Lista_zf.reshape(8, qtd_pts)
+    Lista_xf_reshaped = Lista_xf.reshape(14, qtd_pts)
 
     # criando as pertubações de u0
-    valve_open1 = np.random.uniform(.42, 1, n_pert)
-    valve_open2 = np.random.uniform(.42, 1, n_pert)
-    valve_open3 = np.random.uniform(.42, 1, n_pert)
-    valve_open4 = np.random.uniform(.42, 1, n_pert)
-    # bcs_freq1 = np.random.randint(35., 65., n_pert)
-    # bcs_freq2 = np.random.randint(35., 65., n_pert)
-    # bcs_freq3 = np.random.randint(35., 65., n_pert)
-    # bcs_freq4 = np.random.randint(35., 65., n_pert)
-    booster_freq = np.random.uniform(35., 65., n_pert)
+    base_valve_open = 0.5
+    base_bcs = 50.
+    delta = 0.05
+    delta_bcs = 5
+    valve_open1 = np.clip(base_valve_open + np.random.uniform(-delta, delta, n_pert), 0, 1.0)
+    valve_open2 = np.clip(base_valve_open + np.random.uniform(-delta, delta, n_pert), 0, 1.0)
+    valve_open3 = np.clip(base_valve_open + np.random.uniform(-delta, delta, n_pert), 0, 1.0)
+    valve_open4 = np.clip(base_valve_open + np.random.uniform(-delta, delta, n_pert), 0, 1.0)
+    bcs_freq1 = np.clip(base_bcs + np.random.uniform(-delta_bcs, delta_bcs, n_pert), 35, 65)
+    bcs_freq2 = np.clip(base_bcs + np.random.uniform(-delta_bcs, delta_bcs, n_pert), 35, 65)
+    bcs_freq3 = np.clip(base_bcs + np.random.uniform(-delta_bcs, delta_bcs, n_pert), 35, 65)
+    bcs_freq4 = np.clip(base_bcs + np.random.uniform(-delta_bcs, delta_bcs, n_pert), 35, 65)
+    booster_freq = np.clip(base_bcs + np.random.uniform(-delta_bcs, delta_bcs, n_pert), 35, 65)
     p_topo = np.random.uniform(8, 12, n_pert)
 
     grid_cont = 1
     for i in range(n_pert):
         grid_cont += 1
         delta = 1000
-        grid = linspace(tfinal, tfinal + delta, 100)
+        grid = linspace(tfinal, tfinal + delta, qtd_pts)
         tfinal += delta
-        u0 = [booster_freq[i], p_topo[i] ** 5, 50., valve_open1[i], 50., valve_open2[i], 50., valve_open3[i], 50.,
-              valve_open4[i]]
+        u0 = [booster_freq[i], p_topo[i] ** 4, bcs_freq1[i], valve_open1[i], bcs_freq2[i], valve_open2[i], bcs_freq3[i], valve_open3[i], bcs_freq4[i], valve_open4[i]]
         res = F(x0=x0, z0=z0, p=u0)
         x0 = res["xf"][:, -1]
         z0 = res["zf"][:, -1]
@@ -279,7 +280,7 @@ def plotar_graficos(n_pert):
 
         #Plotted Graphs
         rcParams['axes.formatter.useoffset'] = False
-        grid = linspace(0, tfinal, 100 * grid_cont)
+        grid = linspace(0, tfinal, qtd_pts * grid_cont)
 
     def Auto_plot(i, t, xl, yl, c):
         plt.plot(grid, i.transpose(), c)
@@ -292,229 +293,13 @@ def plotar_graficos(n_pert):
         plt.grid()
         plt.show()
 
-    Auto_plot(Lista_zf_reshaped[[1, 3, 5, 7], :], "Pressure Discharge in ESP's", 'Time/(h)', 'Pressure/(bar)', 'b')
-    Auto_plot(Lista_xf_reshaped[[2, 5, 8, 11], :], "Pressure fbhp in ESP's", 'Time/(h)', 'Pressure/(bar)', 'r')
-    Auto_plot(Lista_xf_reshaped[[3, 6, 9, 12], :], 'Pressure in Chokes', 'Time/(h)', 'Pressure/(bar)', 'g')
-    Auto_plot(Lista_xf_reshaped[[4, 7, 10, 13], :], 'Average Flow in the Wells', 'Time/(h)', 'Flow Rate/(m^3/h)', 'k')
-    Auto_plot(Lista_xf_reshaped[[1], :], 'Flow Through the Transportation Line', 'Time/(h)', 'Flow Rate/(m^3/h)','y')
-    Auto_plot(Lista_xf_reshaped[[0], :], 'Manifold Pressure', 'Time/(h)', 'Pressure/(bar)', 'm')
+    # Auto_plot(Lista_zf_reshaped[[1, 3, 5, 7], :], "Pressão de Saída BCS", 'Time/(h)', 'Pressure/(bar)', 'b')
+    # Auto_plot(Lista_xf_reshaped[[2, 5, 8, 11], :], "Pressure de Fundo de Poço", 'Time/(h)', 'Pressure/(bar)', 'r')
+    # Auto_plot(Lista_xf_reshaped[[3, 6, 9, 12], :], 'Pressão da Choke', 'Time/(h)', 'Pressure/(bar)', 'g')
+    # Auto_plot(Lista_xf_reshaped[[4, 7, 10, 13], :], 'Vazão dos Poços', 'Time/(h)', 'Flow Rate/(m^3/h)', 'k')
+    # Auto_plot(Lista_xf_reshaped[[1], :], 'Vazão Manifold', 'Time/(h)', 'Flow Rate/(m^3/h)','y')
+    # Auto_plot(Lista_xf_reshaped[[0], :], 'Pressão Manifold', 'Time/(h)', 'Pressure/(bar)', 'm')
 
-    #p_intake é desnecessário
-    # Auto_plot(Lista_zf_reshaped[[0, 2, 4, 6], :],"Pressure Intake in ESP's", 'Time/(h)', 'Pressure/(bar)')
-
-
-
-
- # %% mapeando estacionários:
-def mapping_stationary(n_pert):
-
-    valve_open1 = np.random.uniform(.1, .9, n_pert)
-    valve_open2 = np.random.uniform(.1, .9, n_pert)
-    valve_open3 = np.random.uniform(.1, .9, n_pert)
-    valve_open4 = np.random.uniform(.1, .9, n_pert)
-    bcs_freq1 = np.random.uniform(35., 65., n_pert)
-    bcs_freq2 = np.random.uniform(35., 65., n_pert)
-    bcs_freq3 = np.random.uniform(35., 65., n_pert)
-    bcs_freq4 = np.random.uniform(35., 65., n_pert)
-    booster_freq = np.random.uniform(35., 65., n_pert)
-    p_topo = np.random.uniform(0.8, 1.2, n_pert)
-
-
-    valves_rand = [valve_open1, valve_open2, valve_open3, valve_open4]
-    bcs_rand = [bcs_freq1, bcs_freq2, bcs_freq3, bcs_freq4]
-    booster_rand = booster_freq
-    p_topo_rand = p_topo
-
-    global x0
-    global z0
-
-    est_P_man = []
-    est_q_tr = []
-    est_P_fbhp1 = []
-    est_P_fbhp2 = []
-    est_P_fbhp3= []
-    est_P_fbhp4 = []
-    est_P_choke1 = []
-    est_P_choke2 = []
-    est_P_choke3 = []
-    est_P_choke4 = []
-    est_q_main1 = []
-    est_q_main2 = []
-    est_q_main3 = []
-    est_q_main4 = []
-    est_P_intake1 = []
-    est_P_intake2 = []
-    est_P_intake3 = []
-    est_P_intake4 = []
-    est_dP_bcs1 = []
-    est_dP_bcs2 = []
-    est_dP_bcs3 = []
-    est_dP_bcs4 = []
-    flag = []
-
-    i = 0
-    contador = 0
-    while contador < n_pert :
-        u0 = [booster_freq[i], p_topo[i] * 10 ** 5, bcs_freq1[i], valve_open1[i], bcs_freq2[i], valve_open2[i], bcs_freq3[i], valve_open3[i], bcs_freq4[i],valve_open4[i]]
-        mani_solver = lambda y: array([float(i) for i in mani.model(0, y[0:-8], y[-8:], u0)])
-        y_ss = fsolve(mani_solver, x0+z0)
-        z_ss = y_ss[-8:]
-        x_ss = y_ss[0:-8]
-        if  x_ss[1] == 340:
-            valve_open1[i] = np.random.uniform(0, 1)
-            valve_open2[i] = np.random.uniform(0, 1)
-            valve_open3[i] = np.random.uniform(0, 1)
-            valve_open4[i] = np.random.uniform(0, 1)
-            bcs_freq1[i] = np.random.uniform(35., 65.)
-            bcs_freq2[i] = np.random.uniform(35., 65.)
-            bcs_freq3[i] = np.random.uniform(35., 65.)
-            bcs_freq4[i] = np.random.uniform(35., 65.)
-            booster_freq[i] = np.random.uniform(35., 65.)
-            p_topo[i] = np.random.uniform(0.8, 1.2)
-            continue
-        else:
-            i += 1
-
-        a_min = (206.6-58.07)/(28.55-20.77)
-        b_min = 58.07-a_min*20.55
-
-        a_max = (170.0983885726676 - 44.8570768595651) / (82.07399108766865 - 53.82716056845215)
-        b_max = 44.8570768595651 - a_max * 53.82716056845215
-
-        qmin1 = (z_ss[1] - b_min) / a_min
-        qmax1 = (z_ss[1] - b_max) / a_max
-        qmin2 = (z_ss[3] - b_min) / a_min
-        qmax2 = (z_ss[3] - b_max) / a_max
-        qmin3 = (z_ss[5] - b_min) / a_min
-        qmax3 = (z_ss[5] - b_max) / a_max
-        qmin4 = (z_ss[7] - b_min) / a_min
-        qmax4 = (z_ss[7] - b_max) / a_max
-        restqmain1 = x_ss[4] >= qmin1 and x_ss[4] <= qmax1
-        restqmain2 = x_ss[7] >= qmin2 and x_ss[7] <= qmax2
-        restqmain3 = x_ss[10] >= qmin3 and x_ss[10] <= qmax3
-        restqmain4 = x_ss[13] >= qmin4 and x_ss[13] <= qmax4
-
-
-        # Adicionando na lista se for positivo
-
-        if x_ss[0] > 0 and restqmain1 and restqmain2 and restqmain3 and restqmain4:
-            Flag = 1
-        else:
-            Flag = 0
-        if x_ss[2] < 74.14291355695116 or x_ss[5] < 74.14291355695116 or \
-                x_ss[8] < 74.14291355695116 or x_ss[11] < 74.14291355695116 and Flag == 1:
-            Flag = 0
-        flag.append(Flag)
-        est_P_man.append(x_ss[0])
-        est_q_tr.append(x_ss[1])
-        est_P_fbhp1.append(x_ss[2])
-        est_P_fbhp2.append(x_ss[5])
-        est_P_fbhp3.append(x_ss[8])
-        est_P_fbhp4.append(x_ss[11])
-        est_P_choke1.append(x_ss[3])
-        est_P_choke2.append(x_ss[6])
-        est_P_choke3.append(x_ss[9])
-        est_P_choke4.append(x_ss[12])
-        est_q_main1.append(x_ss[4])
-        est_q_main2.append(x_ss[7])
-        est_q_main3.append(x_ss[10])
-        est_q_main4.append(x_ss[13])
-        est_P_intake1.append(z_ss[0])
-        est_P_intake2.append(z_ss[2])
-        est_P_intake3.append(z_ss[4])
-        est_P_intake4.append(z_ss[6])
-        est_dP_bcs1.append(z_ss[1])
-        est_dP_bcs2.append(z_ss[3])
-        est_dP_bcs3.append(z_ss[5])
-        est_dP_bcs4.append(z_ss[7])
-        contador += 1
-        print(f'--{contador}/{n_pert}--')
-    # Plotando o Gráfico
-
-    plt.figure(dpi=250)
-    plt.plot(np.array(est_q_tr)[np.array(flag)==0], np.array(est_P_man)[np.array(flag)==0], 'r.')
-    plt.plot(np.array(est_q_tr)[np.array(flag)==1], np.array(est_P_man)[np.array(flag)==1], 'b.')
-    plt.plot([110, 225], [0, 0], 'k--', linewidth=3)
-    plt.xlabel(r"$q_{tr}$ /(m$^3 \cdot$ h$^{-1}$)", fontsize=15)
-    plt.ylabel('$P_{man}$ /bar',fontsize = 15)
-    plt.grid()
-    plt.show()
-
-    plt.figure(dpi=250)
-    plt.plot(np.array(est_q_main1)[np.array(flag)==0], np.array(est_dP_bcs1)[np.array(flag)==0], 'r.')
-    plt.plot(np.array(est_q_main1)[np.array(flag)==1], np.array(est_dP_bcs1)[np.array(flag)==1], 'b.')
-    plt.plot([28.55, 20.77], [206.6, 58.07], 'k--', linewidth=3)
-    plt.plot([82.1, 53.6], [170.1, 44.7], 'k--', linewidth=3)
-    plt.xlabel(r"$q_{main1}$ /(m$^3 \cdot$ h$^{-1}$)", fontsize=15)
-    plt.ylabel('$dP_{bcs1}$ /bar',fontsize = 15)
-    plt.grid()
-    plt.show()
-
-    plt.figure(dpi=250)
-    plt.plot(np.array(est_q_main2)[np.array(flag)==0], np.array(est_dP_bcs2)[np.array(flag)==0], 'r.')
-    plt.plot(np.array(est_q_main2)[np.array(flag)==1], np.array(est_dP_bcs2)[np.array(flag)==1], 'b.')
-    plt.plot([28.55, 20.77], [206.6, 58.07], 'k--', linewidth=3)
-    plt.plot([82.1, 53.6], [170.1, 44.7], 'k--', linewidth=3)
-    plt.xlabel(r"$q_{main2}$ /(m$^3 \cdot$ h$^{-1}$)", fontsize=15)
-    plt.ylabel('$dP_{bcs2}$ /bar',fontsize = 15)
-    plt.grid()
-    plt.show()
-
-    plt.figure(dpi=250)
-    plt.plot(np.array(est_q_main3)[np.array(flag)==0], np.array(est_dP_bcs3)[np.array(flag)==0], 'r.')
-    plt.plot(np.array(est_q_main3)[np.array(flag)==1], np.array(est_dP_bcs3)[np.array(flag)==1], 'b.')
-    plt.plot([28.55, 20.77], [206.6, 58.07], 'k--', linewidth=3)
-    plt.plot([82.1, 53.6], [170.1, 44.7], 'k--', linewidth=3)
-    plt.xlabel(r"$q_{main3}$ /(m$^3 \cdot$ h$^{-1}$)", fontsize=15)
-    plt.ylabel('$dP_{bcs3}$ /bar',fontsize = 15)
-    plt.grid()
-    plt.show()
-
-    plt.figure(dpi=250)
-    plt.plot(np.array(est_q_main4)[np.array(flag)==0], np.array(est_dP_bcs4)[np.array(flag)==0], 'r.')
-    plt.plot(np.array(est_q_main4)[np.array(flag)==1], np.array(est_dP_bcs4)[np.array(flag)==1], 'b.')
-    plt.plot([28.55, 20.77],[206.6, 58.07], 'k--', linewidth=3)
-    plt.plot([82.1, 53.6], [170.1, 44.7], 'k--', linewidth=3)
-    plt.xlabel(r"$q_{main2}$ /(m$^3 \cdot$ h$^{-1}$)", fontsize=15)
-    plt.ylabel('$dP_{bcs4}$ /bar',fontsize = 15)
-    plt.grid()
-    plt.show()
-
-    dados = {
-
-        'p_topo': p_topo_rand.tolist(),
-        'valve1': valve_open1,
-        'valve2': valve_open2,
-        'valve3': valve_open3,
-        'valve4': valve_open4,
-        'bcs1_freq': bcs_freq1,
-        'bcs2_freq': bcs_freq2,
-        'bcs3_freq': bcs_freq3,
-        'bcs4_freq': bcs_freq4,
-        'booster_freq': booster_rand.tolist(),
-        'P_man': est_P_man,
-        'P_fbhp1': est_P_fbhp1,
-        'P_fbhp2': est_P_fbhp2,
-        'P_fbhp3': est_P_fbhp3,
-        'P_fbhp4': est_P_fbhp4,
-        'P_choke1': est_P_choke1,
-        'P_choke2': est_P_choke2,
-        'P_choke3': est_P_choke3,
-        'P_choke4': est_P_choke4,
-        'P_intake1': est_P_intake1,
-        'P_intake2': est_P_intake2,
-        'P_intake3': est_P_intake3,
-        'P_intake4': est_P_intake4,
-        'dP_bcs1': est_dP_bcs1,
-        'dP_bcs2': est_dP_bcs2,
-        'dP_bcs3': est_dP_bcs3,
-        'dP_bcs4': est_dP_bcs4,
-        'q_main1': est_q_main1,
-        'q_main2': est_q_main2,
-        'q_main3': est_q_main3,
-        'q_main4': est_q_main4,
-        'q_tr': est_q_tr,
-        'flag': flag,
-    }
-    return dados
-
+    # p_intake é desnecessário
+    # Auto_plot(Lista_zf_reshaped[[0, 2, 4, 6], :],"Pressão de Entrada BCS", 'Time/(h)', 'Pressure/(bar)', 'c')
+    return Lista_xf_reshaped, Lista_zf_reshaped
